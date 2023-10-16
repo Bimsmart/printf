8a6dd7a23024cc0d6d3a3a80b2a68ed8cc704a4b
codemayor19 <codemayor19@gmail.com>
2023-10-16 04:38:06 -0700

    0-printf.c file

---
+++ 0-printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - prints value to the standard output
+ * @format: string value
+ * @...: other argumets
+ *
+ * Return: integer
+ */
+int _printf(const char *format, ...)
+{
+
+}
