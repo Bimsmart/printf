8760bc6565c6f02b789883db84ef198456b859ae
codemayor19 <codemayor19@gmail.com>
2023-10-17 04:48:53 -0700

    new commit

--- 0-printf.c
+++ 0-printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	int num = 0;
 
 	va_list(printme);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 			return (-1);
 	va_start(printme, format);
 	for (; *format; format++)
