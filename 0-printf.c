2e4fb995fdb03d335fe2a108a175a112c0662f76
Bimsmart <bolaewenla66@gmail.com>
2023-10-18 08:29:14 -0700

    new

--- 0-printf.c
+++ 0-printf.c
@@ -1,6 +1,19 @@
 #include "main.h"
+#include <unistd.h>
 #include <string.h>
 #include <stdarg.h>
+/**
+ * handle_write - this function hadles write
+ * @n: a pointer to no of characters
+ * @str: a pointer to a string
+ * @len: length of string
+ * returns: void
+ */
+void handle_write(int *n, char *str, int len)
+{
+	write(1, str, len);
+	*n += len;
+}
 /**
  * _printf - prints value to the standard output
  * @format: string value
@@ -10,42 +23,41 @@
  */
 int _printf(const char *format, ...)
 {
-	int num = 0;
+	int num = 0, len, number;
+	char buffer[1024];
 
 	va_list(printme);
 	if (format == NULL)
-			return (-1);
+		return (-1);
 	va_start(printme, format);
 	for (; *format; format++)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			num++;
+			handle_write(&num, format, 1);
 		} else
 		{
 			format++;
 			if (*format == '\0')
 				break;
 			if (*format == '%')
-			{
-				write(1, format, 1);
-				num++;
-			}
+				handle_write(&num, format, 1);
 			if (*format == 'c')
 			{
 				char c = va_arg(printme, int);
 
-				write(1, &c, 1);
-				num++;
+				handle_write(&num, &c, 1);
 			}
 			if (*format == 's')
 			{
 				char *s = va_arg(printme, char *);
 
-				write(1, s, strlen(s));
-				num += strlen(s);
+				handle_write(1, s, strlen(s));
 			}
+			if (*format == 'i' || *format == 'd')
+			number = va_arg(printme, int);
+			len = sprint(buffer, "%d", num);
+			fwrite(buffer, 1, len, stdout);
 		}
 	}
 	va_end(printme);
